02a11fc68cbbf2ffc791e793ef8c39f6f6aa7f6a
Gleb Stepanov <glebstepanov1992@gmail.com>
2021-08-19 01:17:25 +0300

    Add more effective buy and sell stocks implementation

--- arrays/buy_and_sell_stocks_k_times.py
+++ arrays/buy_and_sell_stocks_k_times.py
@@ -1,6 +1,7 @@
 # Given an array of stock prices over the time
 # and k - max number of transactions to settle.
 # Find out maximum profit from these at most K transactions.
+# O(N^3) implementation.
 def buy_and_sell_stocks_k_times(prices, k):
     profit = [[0 for _ in range(len(prices) + 1)] for _ in range(k + 1)]
 
@@ -14,5 +15,19 @@ def buy_and_sell_stocks_k_times(prices, k):
     return profit[k][len(prices) - 1]
 
 
+# More effective O(N^2) implementation.
+def buy_and_sell_stocks_k_times2(prices, k):
+    profit = [[0 for _ in range(len(prices) + 1)] for _ in range(k + 1)]
+
+    for i in range(1, k + 1):
+        prev_diff = -1000
+        for j in range(1, len(prices)):
+            prev_diff = max(prev_diff, profit[i - 1][j - 1] - prices[j - 1])
+            profit[i][j] = max(prev_diff + prices[j], profit[i][j - 1])
+
+    return profit[k][len(prices) - 1]
+
+
 if __name__ == "__main__":
     print(buy_and_sell_stocks_k_times([10, 22, 5, 75, 65, 80], 2))
+    print(buy_and_sell_stocks_k_times2([10, 22, 5, 75, 65, 80], 2))
