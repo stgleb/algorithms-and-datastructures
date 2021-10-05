db85a31759b5a007a1a61eeabbeb33a6bc4d44c8
Gleb Stepanov <glebstepanov1992@gmail.com>
2021-10-05 19:13:25 +0300

    Find first node in cycle

--- lists/test_for_cycle.py
+++ lists/test_for_cycle.py
@@ -23,6 +23,31 @@ def test_for_cycles(head):
     return True
 
 
+def first_node_in_cycle(head):
+    fast, slow = head, head
+    fast = fast.next
+    if fast:
+        fast = fast.next
+    slow = slow.next
+
+    while fast and slow and fast != slow:
+        fast = fast.next
+        if fast:
+            fast = fast.next
+        slow = slow.next
+
+    if fast == slow and fast is not None:
+        fast = head
+
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+
+        return fast.val
+
+    return None
+
+
 if __name__ == "__main__":
     n1 = Node(1)
     n2 = Node(2)
@@ -37,5 +62,7 @@ if __name__ == "__main__":
     n4.next = n5
     n5.next = n6
     n6.next = n7
+    n7.next = n4
 
     print(test_for_cycles(n1))
+    print(first_node_in_cycle(n1))
