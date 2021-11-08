61eaa07306ab9a89a6f5654201fb38415f540472
Gleb Stepanov <glebstepanov1992@gmail.com>
2021-11-09 00:26:07 +0200

    palindrome decomposition

---
+++ recursion/palindrome_decomposition.py
@@ -0,0 +1,17 @@
+def palindrome_decomposition(s):
+    def decompose_util(text, decomposition, result):
+        if len(text) == 0:
+            result.append(decomposition.copy())
+        for i in range(1, len(text) + 1):
+            if text[:i] == ''.join(reversed(text[:i])):
+                decomposition.append(text[:i])
+                decompose_util(text[i:], decomposition, result)
+                decomposition.pop()
+
+    result = []
+    decompose_util(s, [], result)
+    return result
+
+
+if __name__ == "__main__":
+    print(palindrome_decomposition("1210057887"))
