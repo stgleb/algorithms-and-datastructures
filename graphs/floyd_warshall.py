053a44725a79f52b20609df4add4d30d8d7460fc
Gleb Stepanov <glebstepanov1992@gmail.com>
2021-11-18 22:19:19 +0200

    Floyd Warshall

---
+++ graphs/floyd_warshall.py
@@ -0,0 +1,27 @@
+def shortest_paths(graph):
+    n = len(graph)
+    dist = [row[:] for row in graph]
+    for i in range(n):
+        for j in range(n):
+            if i != j and dist[i][j] == 0:
+                dist[i][j] = float('inf')
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist
+
+
+if __name__ == "__main__":
+    graph = [
+        [0, 4, 0, 0, 0, 0, 0, 8, 0],
+        [4, 0, 8, 0, 0, 0, 0, 11, 0],
+        [0, 8, 0, 7, 0, 4, 0, 0, 2],
+        [0, 0, 7, 0, 9, 14, 0, 0, 0],
+        [0, 0, 0, 9, 0, 10, 0, 0, 0],
+        [0, 0, 4, 14, 10, 0, 2, 0, 0],
+        [0, 0, 0, 0, 0, 2, 0, 1, 6],
+        [8, 11, 0, 0, 0, 0, 1, 0, 7],
+        [0, 0, 2, 0, 0, 0, 6, 7, 0]
+    ]
+    print(shortest_paths(graph))
