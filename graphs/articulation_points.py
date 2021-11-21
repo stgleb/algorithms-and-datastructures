3ba562da29272447a1f798f1924bb0977a84bd8a
Gleb Stepanov <glebstepanov1992@gmail.com>
2021-11-21 15:13:27 +0200

    articulation point

--- graphs/articulation_points.py
+++ graphs/articulation_points.py
@@ -1,23 +1,24 @@
 def articulation_points_util(graph, u, visited, disc, low, parent, time, result):
-    time[0] += 1
     visited[u] = True
     disc[u] = time[0]
     low[u] = time[0]
+    time[0] += 1
     children = 0
-    for v in graph[u]:
-        if v != 0:
-            children += 1
-    for v, _ in enumerate(graph[u]):
-        if not visited[v]:
-            parent[v] = u
-            articulation_points_util(graph, v, visited, disc, low, parent, time, result)
-            low[u] = min(low[u], low[v])
-            if parent[u] == -1 and children > 1:
-                result.append(u)
-            if parent[u] != -1 and low[v] > disc[u]:
-                result.append(u)
-        elif v != parent[u]:
-            low[u] = min(low[u], disc[v])
+    for v, w in enumerate(graph[u]):
+        if w:
+            if v == parent[u]:
+                continue
+            elif visited[v]:
+                low[u] = min(low[u], disc[v])
+            else:
+                children += 1
+                parent[v] = u
+                articulation_points_util(graph, v, visited, disc, low, parent, time, result)
+                low[u] = min(low[u], low[v])
+                if parent[u] != -1 and low[v] >= disc[u]:
+                    result.append(u)
+    if parent[u] == -1 and children > 1:
+        result.append(u)
 
 
 def articulation_points(graph):
@@ -35,12 +36,10 @@ def articulation_points(graph):
 
 if __name__ == "__main__":
     graph = [
-        [0, 1, 1, 0, 0, 0, 0],
-        [1, 0, 1, 0, 1, 0, 1],
-        [1, 1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 1, 0],
-        [0, 1, 0, 0, 0, 1, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [1, 0, 1, 0, 0],
+        [1, 1, 0, 1, 1],
+        [0, 0, 1, 0, 1],
+        [0, 0, 1, 1, 0],
     ]
     print(articulation_points(graph))
